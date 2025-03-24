2e6a3f6b9474d88ae65a8f671b0bd3377afc7c21
Taehyeony <teahyun0540@gmail.com>
2025-03-24 17:28:02 +0900

    init

---
+++ src/main/java/com/nextstep/ansan_lms_back/AnsanLmsBackApplication.java
@@ -0,0 +1,13 @@
+package com.nextstep.ansan_lms_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsanLmsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsanLmsBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextstep/ansan_lms_back/AnsanLmsBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextstep.ansan_lms_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsanLmsBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
